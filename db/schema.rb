42f4630a28019f53f4592ae31c26a1b147156305
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-06-01 16:32:23 +0900

    testapp: added user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601071109) do
+ActiveRecord::Schema.define(version: 20170601072133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20170601071109) do
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
     t.string   "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "kana"
+    t.string   "email"
+    t.text     "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
