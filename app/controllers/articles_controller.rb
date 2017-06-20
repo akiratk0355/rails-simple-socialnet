596c5c4bc8706213a4a68fb3c348355ce9836a2f
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-06-20 11:35:07 +0900

    controllers/articles: added #create and #show actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,25 @@
 class ArticlesController < ApplicationController
+  def index
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
   def new
   end
+  def edit
+  end
   def create
-    render plain: params[:article].inspect
+    #render plain: params[:article].inspect
+    @article = Article.new
+    @article.update_attributes(article_params)
+    @article.save
+    redirect_to @article
+  end
+  def update
+  end
+  
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
