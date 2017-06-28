84ad93fdeeb8a61ccd69b2ad41e03ce14a533359
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-06-29 02:37:45 +0900

    models/user: validated a role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
+USER_ROLES = ['user', 'admin']
+
 class User < ActiveRecord::Base
   has_many :articles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+  
+  validates :role, :inclusion => USER_ROLES
+  
   def can_admin?
     if role == "admin"
       true
