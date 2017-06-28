fb694b2c845d3224d3355de60bd0776fc3483869
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-06-29 02:32:13 +0900

    models/article: added do_publish method and scopes

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,21 @@ class Article < ActiveRecord::Base
     has_many :comments, dependent: :destroy
     validates :title, presence: true
     
+    before_save :do_publish
+    
+    scope :published, -> { where(published: true) }
+    scope :unpublished, -> { where(published: false) }
+    
+    # class method for handling search queries
     def self.search(search)
       where("title ILIKE ? OR text ILIKE ?", "%#{search}%", "%#{search}%") 
+    end
+    
+    def do_publish
+      if self.published == true
+        self.published_at = Time.now
+      else
+        self.published_at = nil
+      end
     end
 end
