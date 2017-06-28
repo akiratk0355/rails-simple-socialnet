b2134f2d8aa0f24b021b8d3e710c2afb33775231
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-06-29 03:16:29 +0900

    views/application: added current user status to the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def user_html_string(user)
+    if user.nil?
+      "You are not logged in"
+    else
+      super_string = ""
+      if user.can_admin?
+        super_string = " &mdash; <b style='color: red'>Superuser</b>"
+      end
+      "#{user.name} (#{user.email})" + super_string
+    end
+  end
 end
