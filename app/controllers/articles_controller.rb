1facbdc9e210f3a7d72a2845c474a62c4a584e7c
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-06-29 02:40:19 +0900

    articles#crerate/update: submitted published flag

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,6 +54,6 @@ class ArticlesController < ApplicationController
   
   private
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :published)
   end
 end
