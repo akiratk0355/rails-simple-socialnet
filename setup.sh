7fe1bbe4607936b06ea5bfe6d7c5b7ddda27f665
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-06-29 23:29:14 +0900

    Removed unused setup script

--- setup.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update
-sudo apt -y install nodejs libcurl4-openssl-dev apache2-dev libapr1-dev libaprutil1-dev libxml2 libxslt-dev build-essential patch libssl-dev
-sudo apt -y install sqlite3 libsqlite3-dev
-sudo apt -y install postgresql libpq-dev
