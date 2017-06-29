e81ec77d6c90e1724ae4dd1919dd88f84c95493a
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-06-29 18:14:56 +0900

    Added pg_dump'ed db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,36 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Create admin user
-unless User.where(:email => "admin@example.com").any?
-  admin = User.new :email => "admin@example.com",
-                  :name => "ADMIN",
-                  :password => "boobooboo",
-                  :role => 'admin'
-  admin.save!
-end
-
-unless User.where(:email => "alice@example.com").any?
-  admin = User.new :email => "alice@example.com",
-                  :name => "Alice",
-                  :password => "boobooboo",
-                  :role => 'user'
-  admin.save!
-end
-
-unless User.where(:email => "bob@example.com").any?
-  admin = User.new :email => "bob@example.com",
-                  :name => "Bob",
-                  :password => "boobooboo",
-                  :role => 'user'
-  admin.save!
-end
-
-unless User.where(:email => "charlie@example.com").any?
-  admin = User.new :email => "charlie@example.com",
-                  :name => "Charlie",
-                  :password => "boobooboo",
-                  :role => 'user'
-  admin.save!
-end
